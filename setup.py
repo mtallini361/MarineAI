e7804aead6f05e45b8cb571a6c5163f4a9f3f7df
Michael Tallini <michael.tallini@90north.health>
2023-12-08 16:18:33 -0500

    added requirements to setup

--- setup.py
+++ setup.py
@@ -1,4 +1,23 @@
+from typing import List
 from setuptools import find_packages, setup
+
+HYPHEN_E_DOT = '-e .'
+
+def get_requirements(req_path: str) -> List[str]:
+    '''
+    this function is to pull the required packages from requirements.txt
+    '''
+
+    requirements = []
+    with open(req_path, "r") as infile:
+        requirements = infile.readlines()
+        requirements = [req.strip() for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
+    return requirements
+
 
 setup(
     name='marine_ai',
@@ -7,5 +26,5 @@ setup(
     author='Michael'
     author_email='mtallini361@gmail.com',
     packages=find_packages(),
-    install_requires=['numpy', 'pandas']
+    install_requires=get_requirements('requirements.txt')
 )
