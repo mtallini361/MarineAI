b9f68418d193e9238170f90c4126b1729d7f8964
Michael Tallini <michael.tallini@90north.health>
2023-12-08 16:00:15 -0500

    added setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='marine_ai',
+    version='0.0.1',
+    # TODO: Need to add George also
+    author='Michael'
+    author_email='mtallini361@gmail.com',
+    packages=find_packages(),
+    install_requires=['numpy', 'pandas']
+)
