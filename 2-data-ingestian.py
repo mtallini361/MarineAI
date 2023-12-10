930b02506fc0aad7dfe5f8cdf7b5d3095df6d1e5
gmonez26 <georgemonez1997@gmail.com>
2023-12-10 11:53:30 -0500

    Once the proper website url is put into line 5, this should scan all images on said website and download them into a file called MarineAI_image_bank.

--- 2-data-ingestian.py
+++ 2-data-ingestian.py
@@ -1,17 +1,18 @@
 import bs4
 import requests
 from bs4 import BeautifulSoup
-
+#need to input URL of chosen website for data collection.
 req = requests.get("INSTALL_WEBSITE_TO_BE_SCRAPED_HERE")
 
 soup = BeautifulSoup(req.content,"html.parser")
-
+#find picture
 for img in soup.findAll("img"):
     if img.get("src") != None:
         print (img.get("src"))
         img_url = img.get("src")
         name = img_url.split)"/")[-1]
         img_response = requests.get(img_url)
+        #save image to MarineAI_image_bank
         file = open(MarineAI_image_bank, "wb")
         file.write(img_response.content)
         file.close()
